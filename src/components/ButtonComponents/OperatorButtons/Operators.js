22db4beb3c67c1900e8ca9c912bb693ca127f06f
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-11-06 15:47:35 -0600

    Finished Operators.js file.

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,25 +1,23 @@
-import React, { useState } from "react";
-import { operators } from "../../../data.js";
-import OperatorButton from "./OperatorButton";
+import React, { useState } from 'react';
+import { operators } from '../../../data.js';
+import OperatorButton from './OperatorButton';
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
-  console.log(operators);
-  const [operatorData, setOperatorData] = useState(operators);
-  // STEP 2 - add the imported data to state
-  console.log(data);
-  return (
-    <div>
-      {operatorData.map((operator, i) => {
-        return <OperatorButton key = {i} operator = {operator.value}
-      })}
-      /* STEP 3 - Use .map() to iterate over your array data and return a button
+const Operators = (props) => {
+	const [ operatorData, setOperatorData ] = useState(operators);
+	// STEP 2 - add the imported data to state
+	return (
+		<div className='operators'>
+			{operatorData.map((operator, index) => {
+				return <OperatorButton key={index} operator={operator.value} click={props.click} />; //Do I need value here?
+			})}
+			{/* STEP 3 - Use .map() to iterate over your array data and return a button
       component matching the name on the provided file. Pass
-      it any props needed by the child component*/
-    </div>
-  );
+      it any props needed by the child component */}
+		</div>
+	);
 };
 export default Operators;
