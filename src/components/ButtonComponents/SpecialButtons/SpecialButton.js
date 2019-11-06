90074f4e1d2d51c5fd6c110c584cdf4c4c7d805e
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-11-06 15:57:33 -0600

    Finished SpecialButton.js file.

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-const SpecialButton = ({special}) => {
+const SpecialButton = (props) => {
   return (
     <>
-      <button>
-      {special} {/* this is a string */}
-      </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className="button spec-btn" onClick={() => props.click(props.special)}>{props.special}</button>
     </>
   );
 };
-export default SpecialButton
+export default SpecialButton;
