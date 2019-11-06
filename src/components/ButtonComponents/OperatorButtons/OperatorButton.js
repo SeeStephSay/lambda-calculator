0054245adc114b60b88c8111878b7e38c2e9d6cd
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-11-06 15:50:45 -0600

    Finished OperatorButton.js file.

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-const OperatorButton = ({operator}) => {
+const OperatorButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className="button op-btn" onClick={() => props.click(props.operator.value)}>{props.operator.char}</button>
     </>
-    <button>
-      {operator}
-    </button>
   );
 };
 
